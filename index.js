5bd6e938a40b4402d7aa97ca3e2eb2e09c079419
nikolaskorakidis <korakidisn@gmail.com>
2021-01-25 18:04:21 +0100

    Event Listeners part 1

--- index.js
+++ index.js
@@ -70,3 +70,18 @@ observer.observe(targetNode, config);
 
 // Later, you can stop observing
 observer.disconnect();
+
+// Event listeners
+// Function to change the content of t2
+function modifyText() {
+  const t2 = document.getElementById("t2");
+  if (t2.firstChild.nodeValue == "three") {
+    t2.firstChild.nodeValue = "two";
+  } else {
+    t2.firstChild.nodeValue = "three";
+  }
+}
+
+// Add event listener to table
+const el = document.getElementById("outside");
+el.addEventListener("click", modifyText, false);
