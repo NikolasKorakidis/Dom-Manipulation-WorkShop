639bcf9c260e51232d0c86c3089273a3256b1aaf
nikolaskorakidis <korakidisn@gmail.com>
2021-01-26 13:24:37 +0100

    Examples Part 1 Done

--- index.js
+++ index.js
@@ -72,16 +72,35 @@ observer.observe(targetNode, config);
 observer.disconnect();
 
 // Event listeners
+
+// Example One
 // Function to change the content of t2
-function modifyText() {
+// function modifyText() {
+//   const t2 = document.getElementById("t2");
+//   if (t2.firstChild.nodeValue == "three") {
+//     t2.firstChild.nodeValue = "two";
+//   } else {
+//     t2.firstChild.nodeValue = "three";
+//   }
+// }
+
+// // Add event listener to table
+// const el = document.getElementById("outside");
+// el.addEventListener("click", modifyText, false);
+
+// Example Two
+// Function to change the content of t2
+function modifyText(new_text) {
   const t2 = document.getElementById("t2");
-  if (t2.firstChild.nodeValue == "three") {
-    t2.firstChild.nodeValue = "two";
-  } else {
-    t2.firstChild.nodeValue = "three";
-  }
+  t2.firstChild.nodeValue = new_text;
 }
 
-// Add event listener to table
+// Function to add event listener to table
 const el = document.getElementById("outside");
-el.addEventListener("click", modifyText, false);
+el.addEventListener(
+  "click",
+  function () {
+    modifyText("four");
+  },
+  false
+);
